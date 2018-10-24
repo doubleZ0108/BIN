9ffbfc5f258a5fc087303f762bd0d7174a4ef037
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-10-24 17:02:45 +0800

    链表的插入和删除

--- EverydayCode/EverdayCode7.c
+++ EverydayCode/EverdayCode7.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FRONT 1
+#define BACK 0
+
 struct Stu
 {
 	int num;
@@ -38,7 +41,7 @@ void InitLink(const struct NODE *head)
 	for (int i = 1; move != NULL; move = move->next, ++i)
 	{
 		printf("请输入第%d个结点的信息: ", i);
-		scanf("%d", &move->data);
+		scanf("%d", &move->data.num);
 	}
 }
 void OutputLink(const struct NODE *head)
@@ -67,11 +70,67 @@ void DestroyLink(struct NODE *head)
 		printf("*链表已销毁!\n");
 	}
 }
+
+void InsertLink(const struct NODE *head, int flag)
+{
+	struct NODE *move = (flag == FRONT ? head : head->next);
+
+	int index;
+	printf("你想在第几个结点"); printf(flag == FRONT ? "前插入: " : "后插入: ");
+	scanf("%d", &index);
+
+	for (int i = 1; (flag == FRONT ? move->next : move) != NULL; move = move->next, ++i)
+	{
+		if (i == index)
+		{
+			struct NODE *fresh = malloc(sizeof*fresh);
+			printf("请输入新结点的信息: ");
+			scanf("%d", &fresh->data.num);
+
+			fresh->next = move->next;
+			move->next = fresh;
+
+			return;
+		}
+	}
+}
+void DeleteLink(const struct NODE *head)
+{
+	struct NODE *move = head;
+
+	int index;
+	printf("你想删除第几个结点: ");
+	scanf("%d", &index);
+
+	for (int i = 1; move->next != NULL; move = move->next, ++i)
+	{
+		if (i == index)
+		{
+			struct NODE *save = move->next;
+			move->next = move->next->next;
+
+			free(save);
+			save = NULL;
+			
+			return;
+		}
+	}
+
+	printf("无第%d个结点\n", index);
+}
 int main(void)
 {
 	struct NODE *head = CreateLink();
 	InitLink(head);
 	OutputLink(head);
+
+	//InsertLink(head, FRONT);
+	//OutputLink(head);
+	//InsertLink(head, BACK);
+	//OutputLink(head);
+
+	//DeleteLink(head);
+	//OutputLink(head);
 
 
 	DestroyLink(head);
