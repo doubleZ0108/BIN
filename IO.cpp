6cfe3d2d11794a9402d738b23e5b5e34d90830a5
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-11-15 19:43:26 +0800

    C++输入输出 之 cin

--- IO.cpp
+++ IO.cpp
@@ -14,7 +14,7 @@
 
 using namespace std;
 
-void cinANDcout()
+void cout_usage()
 {
 	cout << endl << "double test" << endl;
 	double a = 123.456789012345;
@@ -91,33 +91,8 @@ void Comprehense()
 	cout << pi << endl;
 }
 
-int main(void)
+void cout_put()
 {
-	/*cin cout*/
-	//(1)不能使用cin把空格字符和回车换行符作为字符输入给字符变量, 它们将被跳过
-	//	 如果想把他们两个输入个字符变量, 可以使用getchar函数
-	//(2)width()和setw()只对其后的第一项输入项有效
-	//	 例如cout<<setw(6)<<20<<3.14<<endl; 20前面会有4个空格,3.14按系统默认的宽度输出(数据实际长度)
-	//(3)使用setf和setiosflags后,如果想设置为同组的另一种状态,要先用unsetf或resetionsflags
-	//	 在设置其他状态,否则系统仍然按之前的设置输出
-	//(4)用setf设置格式状态时,可以用委员算符|组合多个格式状态(原因是ios类被定义为枚举值)
-	//	 如 cout.setf(ios::internal | ios::showpos);
-
-	cinANDcout();
-	Comprehense();	//cin和cout的综合应用
-
-	
-
-	/*cerr流对象*/
-	//cerr流已被指定为与显示器关联, 也就意味着cerr流中的信息只能在显示器输出
-	//在调试程序时, 往往不希望程序运行时的出错信息被送到其他文件, 而要求在显示器上及时输出
-	//这是应使用cerr
-	//并且cerr是不经过缓冲区的, 直接向显示器上输出有关信息
-
-
-	/*流成员函数put输出单个字符*/
-	//put成员函数也可以用ostream类的其他流对象调用
-
 	cout << endl << "put: " << endl;
 	cout.put(71).put(79).put(79).put(68).put('\n');
 
@@ -129,6 +104,70 @@ int main(void)
 		cout.put(*(str + i));
 	}
 	cout.put('\n');
+}
+void cin_get()
+{
+	cout << endl << "test for get()" << endl;
+	//不带参数的get函数
+	//char ch;
+	//while ((ch = cin.get()) != EOF)
+	//	//以文件结束表示作为判定, 所以最后要出入 Ctrl+Z 才能终止程序
+	//{
+	//	cout.put(ch);
+	//}
+
+	////有一个参数的get
+	//while (cin.get(ch))
+	//{
+	//	cout.put(ch);
+	//}
+
+	//有3个参数的get
+	//cin.get(字符数组, 字符个数n, 终止字符)
+	//从输入流中读取n-1个字符, 如果在n-1个字符之中就遇到指定的终止符了则提前结束读取
+	char arrch[20];
+	cin.get(arrch, 10, '\\n');		//注意双斜杠
+	cout << arrch << endl;
+}
+
+int main(void)
+{
+	/*cout输出流对象*/
+	//(1)width()和setw()只对其后的第一项输入项有效
+	//	 例如cout<<setw(6)<<20<<3.14<<endl; 20前面会有4个空格,3.14按系统默认的宽度输出(数据实际长度)
+	//(2)使用setf和setiosflags后,如果想设置为同组的另一种状态,要先用unsetf或resetionsflags
+	//	 在设置其他状态,否则系统仍然按之前的设置输出
+	//(3)用setf设置格式状态时,可以用委员算符|组合多个格式状态(原因是ios类被定义为枚举值)
+	//	 如 cout.setf(ios::internal | ios::showpos);
+
+	//cout_usage();
+	//Comprehense();	//cin和cout的综合应用
+
+
+	/*cin输入流*/
+	//(1)不能使用cin把空格字符和回车换行符作为字符输入给字符变量, 它们将被跳过
+	//	 如果想把空格和回车输入成字符变量, 可以使用getchar函数
+	//(2)cin本身的状态就是一个数值, 所以写 while(cin>>temp) 的时候就是用的cin的值
+	//	 当cin遇到无效字符或遇到文件结束符, cin出于出错状态, 值为false
+	//	 比如temp是int类型, 我输入的是23.45, 那么while会执行一次, 然后退出循环
+
+
+
+
+	/*cerr流对象*/
+	//cerr流已被指定为与显示器关联, 也就意味着cerr流中的信息只能在显示器输出
+	//在调试程序时, 往往不希望程序运行时的出错信息被送到其他文件, 而要求在显示器上及时输出
+	//这是应使用cerr
+	//并且cerr是不经过缓冲区的, 直接向显示器上输出有关信息
+
+
+	/*流成员函数put输出单个字符*/
+	//put成员函数也可以用ostream类的其他流对象调用
+	/*cout_put();*/
+	
+	/*流成员函数get读入单个字符*/
+	//
+	cin_get();
 
 
 	system("pause");
