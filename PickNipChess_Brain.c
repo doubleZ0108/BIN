55ea02d9d98c3a0103ebe8acbf4732d0cbb50305
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-11-20 10:44:56 +0800

    挑夹棋1.1 (输出第一个能走的位置)

--- PickNipChess_Brain.c
+++ PickNipChess_Brain.c
@@ -11,6 +11,7 @@ typedef int bool;
 #define ROW 14
 #define COL 14
 //棋子颜色
+#define GAP	  0
 #define BLACK 1
 #define WHITE 2
 
@@ -24,6 +25,54 @@ typedef int bool;
 #define DOWN_LEFT 6
 #define DOWN_RIGHT 7
 
+typedef struct Chess Chess;
+typedef struct LinkList LinkList;
+
+struct Chess
+{
+	int x;
+	int y;
+	int option;
+};
+struct NODE
+{
+	struct Chess data;
+	struct NODE *next;
+};
+struct LinkList
+{
+	struct NODE *head;
+	struct NODE *rear;
+};
+
+void CreateLink(LinkList *list)
+{
+	list->head = list->rear = malloc(sizeof*list->head);
+	struct NODE *move = list->head;
+	move->next = NULL;
+}
+void DestroyLink(LinkList *list)
+{
+	struct NODE *save;
+	while (list->head)
+	{
+		save = list->head;
+		list->head = list->head->next;
+		free(save);
+		save = NULL;
+	}
+}
+void Push_back(LinkList *list, Chess *buf)
+{
+	struct NODE *fresh = malloc(sizeof*fresh);
+	fresh->data = *buf;
+	fresh->next = NULL;
+
+	list->rear->next = fresh;
+	list->rear = list->rear->next;		//更新尾指针
+}
+
+/*显示整个棋盘*/
 void showMap(int Map[][COL])
 {
 	printf("****************************************\n");
@@ -45,6 +94,8 @@ void showMap(int Map[][COL])
 
 	printf("****************************************\n");
 }
+/*移动某方棋子*/
+//不做任何检测
 void setMap(int color, int x, int y, int option, int Map[][COL])
 {
 	switch (option)
@@ -91,11 +142,123 @@ void setMap(int color, int x, int y, int option, int Map[][COL])
 	}
 	default:break;
 	}
-	Map[x][y] = 0;
-}
-void FindNext(int Map[][COL],int Nextx,int Nexty,int Nextoption)
-{
+	Map[x][y] = GAP;
 
+}
+/*某方向是否可走*/
+bool canWalk(int x, int y, int option, int Map[][COL])
+{
+	switch (option)
+	{
+	case UP:
+	{
+		if (x - 1 == 0) { return false; }		//出界判断
+
+		if (Map[x - 1][y] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case DOWN:
+	{
+		if (x + 1 == 13) { return false; }
+
+		if (Map[x + 1][y] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case LEFT:
+	{
+		if (y - 1 == 0) { return false; }
+
+		if (Map[x][y - 1] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case RIGHT:
+	{
+		if (y + 1 == 13) { return false; }
+
+		if (Map[x][y + 1] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case UP_LEFT:
+	{
+		if (x - 1 == 0 || y - 1 == 0) { return false; }
+
+		if (Map[x - 1][y - 1] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case UP_RIGHT:
+	{
+		if (x - 1 == 0 || y + 1 == 13) { return false; }
+
+		if (Map[x - 1][y + 1] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case DOWN_LEFT:
+	{
+		if (x + 1 == 13 || y - 1 == 0) { return false; }
+
+		if (Map[x + 1][y - 1] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	case DOWN_RIGHT:
+	{
+		if (x + 1 == 13 || y + 1 == 13) { return false; }
+
+		if (Map[x + 1][y + 1] == GAP) { return true; }
+		else { return false; }
+
+		break;
+	}
+	default:return false; break;
+	}
+}
+
+/*最高层的函数  用来寻找下一步的落子位置和方向*/
+void FindNext(int Mcolor, int Map[][COL],int *Nextx,int *Nexty,int *Nextoption)
+{
+	LinkList list;
+	CreateLink(&list);
+
+	Chess buf;
+
+	for (int i = 1; i < 13; ++i)
+	{
+		for (int j = 1; j < 13; ++j)
+		{
+			if (Map[i][j] == Mcolor)
+			{
+				for (int option = UP; option <= DOWN_RIGHT; ++option)
+				{
+					if (canWalk(i, j, option, Map))
+					{
+						buf.option = option;
+						buf.x = i; buf.y = j;
+
+						Push_back(&list, &buf);
+					}
+				}
+			}
+		}
+	}
+
+	*Nextx = list.rear->data.x;
+	*Nexty = list.rear->data.y;
+	*Nextoption = list.rear->data.option;
+
+	DestroyLink(&list);
 }
 int main(void)
 {
@@ -131,14 +294,14 @@ int main(void)
 		}
 		else if (OpCode[0] == 'T')
 		{
-			FindNext(Map, x, y, option);
+			FindNext(Mcolor, Map, &x, &y, &option);
 			printf("%d %d %d\n", x-1, y-1, option);		//消除掉多加的一行一列
 			fflush(stdout);
-			setMap(Mcolor, x-1, y-1, option, Map);
+			setMap(Mcolor, x, y, option, Map);		//setMap()不用加一,这个Map就是我自己包了一圈的Map
 		}
 		getchar();
 
-		showMap(Map);
+		//showMap(Map);
 	}
 
 	system("pause");
