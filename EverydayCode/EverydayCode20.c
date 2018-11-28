ac884833298b5894424ea4fa478c5445c47dc0c6
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-11-28 16:43:01 +0800

    链表的插入删除

--- EverydayCode/EverydayCode20.c
+++ EverydayCode/EverydayCode20.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int bool;
+enum{false,true};
+#define FRONT 0
+#define BACK 1
+
 struct Student
 {
 	int num;
@@ -60,7 +65,76 @@ void OutputLink(const struct NODE *head)
 	}
 	printf("[^]\n");
 }
+void DestroyLink(struct NODE *head)
+{
+	struct NODE *save;
+	while (head != NULL)
+	{
+		save = head;
+		head = head->next;
+		free(save);
+		save = NULL;
+	}
 
+	if (!head)
+	{
+		printf("链表已经销毁!\n");
+	}
+}
+
+void InsertLink(const struct NODE *head, bool flag)
+{
+	struct NODE *move = (flag == FRONT ? head : head->next);
+
+	int index;
+	printf("你想在第几个结点之");
+	printf(flag == FRONT ? "前" : "后");
+	printf("插入: ");
+	scanf("%d", &index);
+
+	for (int i=1; (flag == FRONT ? move->next : move) != NULL; ++i,move = move->next)
+	{
+		if (i == index)
+		{
+			struct NODE *fresh = malloc(sizeof*fresh);
+			if (!fresh)
+			{
+				printf("Memory alloction error!\n");
+				exit(-1);
+			}
+
+			printf("请输入插入的新结点信息: ");
+			scanf("%d", &fresh->data.num);
+
+			fresh->next = move->next;
+			move->next = fresh;
+
+			break;
+		}
+	}
+}
+void DeleteLink(const struct NODE *head)
+{
+	struct NODE *move = head;
+
+	int index;
+	printf("你想删除第几个结点: ");
+	scanf("%d", &index);
+
+	for (int i = 1; move->next != NULL; move = move->next, ++i)
+	{
+		if (i == index)
+		{
+			struct NODE *save = move->next;
+			move->next = move->next->next;
+
+			free(save);
+			save = NULL;
+
+			break;
+		}
+	}
+}
 
 int main(void)
 {
@@ -68,6 +142,14 @@ int main(void)
 	InitLink(head);
 	OutputLink(head);
 
+	//InsertLink(head, FRONT);
+	//InsertLink(head, BACK);
+
+	DeleteLink(head);
+	OutputLink(head);
+
+
+	DestroyLink(head);
 	system("pause");
 	return 0;
 }
