8480f46c6935bf2cb309ac9ce00827d72f07e771
double Z <39005525+doubleZ0108@users.noreply.github.com>
2018-12-08 22:02:29 +0800

    穷举法模拟有人说假话时找小偷问题

---
+++ OJ/oj5.c
@@ -0,0 +1,62 @@
+///*
+//Q: "求两个数的 最大公约数 和 最小公倍数"
+//*/
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int GCD_division(int m, int n)
+////辗转相除法
+//{
+//	int buf;
+//	while (buf = m % n)
+//	{
+//		m = n;
+//		n = buf;
+//	}
+//	return n;
+//}
+//int GCD_recrusion(int m, int n)
+////递归法
+//{
+//	if (m%n == 0) { return n; }
+//	else
+//	{
+//		GCD_recrusion(n, m%n);
+//	}
+//}
+//int GCD_subtraction(int m, int n)
+////辗转相减法
+//{
+//	while (m != n)
+//	{
+//		//max(m, n) = max(m, n) - min(m, n);			
+//			//Q: 问号表达式为什么不能作为左值?
+//			//A: 问号表达式最后返回的是一个值, 不是一个对象
+//
+//		if (m > n)
+//		{
+//			m = m - n;
+//		}
+//		else
+//		{
+//			n = n - m;
+//		}
+//	}
+//	return m;
+//}
+//
+//int main(void)
+//{
+//	int m, n;
+//	scanf("%d %d", &m, &n);
+//
+//	printf("辗转相除法解得最大公约数是: %d\n", GCD_division(m,n));
+//	printf("递归解得最大公约数是: %d\n", GCD_recrusion(m, n));
+//	printf("辗转相减法解得最大公约数是: %d\n", GCD_subtraction(m,n));
+//
+//	printf("最小公倍数是: %d\n", m*n / GCD_division(m, n));
+//
+//	system("pause");
+//	return 0;
+//}
