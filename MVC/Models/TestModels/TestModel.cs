483ba91c26bab75fceb3dc0f4bc09fbb00450ba9
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-26 21:47:45 +0800

    添加模型类

---
+++ MVC/Models/TestModels/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myapp.Models.TestModels
+{
+    public class TestModel
+    {
+        public int MyNumber { get; set; }
+    }
+}
