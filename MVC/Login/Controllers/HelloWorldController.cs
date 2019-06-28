8a9bd9a9794c17b53e0d98fc7b4b52b88e92c23a
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-28 22:21:59 +0800

    用户登录(将前端数据读到c#中)

---
+++ MVC/Login/Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.LoginState = "登陆前...";
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(FormCollection formcollection)
+        {
+            string email = formcollection["inputEmail3"];
+            string password = formcollection["inputPassword3"];
+
+            using (System.IO.StreamWriter sw = System.IO.File.AppendText("F:\\coding in VS\\myApp\\log.txt"))
+            {
+                sw.WriteLine("用户输入的email为: "+email);
+                sw.WriteLine("用户输入的password为: "+password);
+            }
+
+
+            ViewBag.LoginState = email + "登陆后...";
+            return View();
+        }
+    }
+}
