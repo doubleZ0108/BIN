ad20152f3da0845d8f33ecaed187919499f5f3b5
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-04 15:34:12 +0800

    数据库期末sql查询练习

--- EverydayCode/DB_review2.sql
+++ EverydayCode/DB_review2.sql
@@ -81,3 +81,133 @@ from student natural join takes
 where year=2009
 group by course_id, sec_id, semester, year
 having count(ID)>2;
+
+--两个时间段同时开的课
+select course_id
+from section
+where semester='Fall' and years=2009
+intersect
+select course_id
+from section
+where semester='Spring' and years=2010;
+
+select course_id
+from section
+where semester='Fall' and years=2009 and course_id in(
+  select course_id
+  from section 
+  where semester='Spring' and years=2010
+);
+
+select S.course_id
+from section S
+where S.semester='Fall' and S.years=2009 and exist(
+  select *
+  from section T
+  where T.semester='Spring' and T.years=2010 and T.course_id=S.course_id
+)
+
+--选修ID为10111老师开课的学生人数
+select count(distinct takes.ID)
+from teaches join takes using(course_id, sec_id, semester, year)
+where teaches.ID='10111';
+
+select count(distinct takes.ID)
+from takes
+where (course_id,sec_id,semester,year) in (
+  select course_id, sec_id, semester, year
+  from teaches
+  where teaches.ID=10111
+);
+
+--生物系开的所有课的学生
+select S.name, S.ID
+from student S
+where not exist(
+  (
+    select course_id
+    from course
+    where dept_name='Biology'
+  )
+  except
+  (
+    select course_id
+    from takes
+    where takes.ID=S.ID
+  )
+);
+
+--2009年最多开设一次的课程
+select course_id
+from section S
+where unique(
+  select course
+  from section T
+  where S.course_id=T.course_id and T.years=2009
+);
+
+select course_id
+from section S
+where 1>=(
+  select count(T.course_id)
+  from section T
+  where T.course_id=S.course_id and T.years=2009
+)
+
+
+--系平均工资大于42000
+select dept_name, avg(salary)
+from instructor
+group by dept_name
+having avg(salary)>42000;
+
+select dept_name, avg_salary
+from (
+  select dept_name, avg(salary) as avg_salary
+  from instructor
+  group by dept_name
+)
+where avg_salary>42000;
+
+--系工资总额最大的系
+select dept_name, sum(salary)
+from instructor
+group by dept_name
+having sum(salary)>=(
+  select max(sum(salary))
+  from instructor
+  group by dept_name
+);
+
+select dept_name, max(sum_salary)
+from (
+  select dept_name, sum(salary) as sum_salary
+  from instructor
+  group by dept_name
+);
+
+with dept_total(dept_name, total_salary) as (
+  select dept_name, sum(salary)
+  from instructor
+  group by dept_name
+)
+select dept_name, total_salary
+from dept_total
+where total_salary>=(
+  select max(total_salary)
+  from dept_total
+);
+
+--所有工资总额大于所有系平均工资总额的系         
+with dept_total(dept_name, value) as(
+  select dept_name, sum(salary)
+  from instructor
+  group by dept_name
+),
+  dept_total_avg(value) as(
+  select avg(value)
+  from dept_total
+)
+select dept_name
+from dept_total, dept_total_avg
+where dept_total.value>dept_total_avg.value;
