0c053ea4090745209b50c0bf032866b150454db0
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-04 10:38:45 +0800

    数据库期末sql查询练习

--- EverydayCode/DB_review2.sql
+++ EverydayCode/DB_review2.sql
@@ -32,3 +32,52 @@ where salary > (
   from instructor
   where dept_name='Comp.Sci.'
 )
+
+--like
+select dept_name, building
+from department
+where building like '%Was%';
+
+--order by
+select *
+from instructor
+order by salary desc, name asc;
+
+--course表有多少元组
+select count(*)
+from course;
+select count(course_id) as count_id
+from course;
+
+--group by
+select dept_name, avg(salary)
+from instructor
+group by dept_name;
+
+select dept_name, count(distinct ID)
+from teaches natural join instructor
+where semester='Spring' and years=2009
+group by dept_name;
+
+
+--平均工资超过42000的所有系
+select dept_name, avg(salary)
+from instructor
+group by dept_name
+having avg(salary)>42000;
+
+with dept_avg(dept_name, value) as(
+  select dept_name, avg(salary)
+  from instructor
+  group by dept_name
+)
+select dept_name, value
+from dept_avg
+where value>42000;
+
+--2019年讲授的每个时间段, 如果该课程有至少2名学生选课, 找出选修它的所有学生的学分平均值
+select course_id,sec_id,semester,year,avg(tot_cred)
+from student natural join takes
+where year=2009
+group by course_id, sec_id, semester, year
+having count(ID)>2;
